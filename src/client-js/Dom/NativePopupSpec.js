903c88e38fb3a27c76ab73da3f987645d87cef5b
Oscar Brito <aetheon@gmail.com>
2013-09-25 11:25:32 +0000

    Moved NativePopup/index to Dom/NativePopup

---
+++ src/client-js/Dom/NativePopupSpec.js
@@ -0,0 +1,44 @@
+
+describe("Dom/NativePopup", function () {
+
+    var Squire = null,
+        Injector = null,
+        async = new AsyncSpec(this);
+
+
+    async.beforeEach(function (done) {
+
+        require(["require", "squire" /*, "js-mocks/MOCK"*/], function(require){
+            
+            Squire = require("squire");
+            Injector = new Squire();
+
+            done();
+
+        });
+
+    });
+
+
+    async.it("module should be loaded", function (done) {
+
+        /*var Mock = require("js-mocks/MOCK");
+
+        Injector.mock('ua-parser', Squire.Helpers.returns(new UAParserMock("Android", "2.0", "tablet")) );
+        */
+        
+        Injector.require(["src/Dom/NativePopup", "jquery"], function(Module, $){
+
+            expect(!!Module).not.toBe(null);
+            //expect("value").toEqual("value");
+
+            done();
+
+        });
+
+    });
+    
+
+
+
+});
