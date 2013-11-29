d7128dd361794aac5bb2ec37ede82668bb19abf1
Oscar Brito <aetheon@gmail.com>
2013-11-29 15:41:56 +0000

    Prepare NPM package

--- src/js-node/index.js
+++ src/js-node/index.js
@@ -2,11 +2,6 @@
 var requirejs = require('requirejs');
 
 requirejs.config({
-    //Pass the top-level main.js/index.js require
-    //function to requirejs so that node modules
-    //are loaded relative to the top-level JS file.
     nodeRequire: require
 });
-
-
 
