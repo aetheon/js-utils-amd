0f47dc85a1cbf8ae8430c475543d7a432147b0b0
Oscar Brito <aetheon@gmail.com>
2013-12-02 17:05:13 +0000

    Refactor js/Arguments

--- src/js/Arguments.js
+++ src/js/Arguments.js
@@ -7,11 +7,132 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
  * 
  */
 
-define(["lodash", "js-utils-lib/Type", "js-utils/DataStructures/Object", "js-utils/Log/index"], function(_, Type, Obj, Log){
+define(["lodash", "js-utils-lib/Type"], function(_, Type){
     "use strict";
 
 
-    var log = new Log.Logger("js-utils/Arguments");
+    /*
+     * Copy all of the objects properties from one object to another if the destination object has the value.
+     * Also, the types are checked.
+     *
+     * @throw{Error} If types are imcompatible.
+     *
+     * @param{obj} The object to update
+     * @param{from} The object data
+     *
+     * @example
+     *
+     *  copyTo({ a: 1, b: 2 }, { a: 0, c: 0 });
+     *  // result will be: { a: 1, c: 0 }
+     *
+     * @return{Object} The merged object
+     */
+    var extend = function(obj, from){
+
+        // if the arguments are not both objects
+        if(! ( Type.of(obj) == "object" && ( Type.of(from) == "object" || Type.of(from) == "undefined") ))
+            throw new Error("[js-utils-lib/Arguments] expected two Objects");
+
+        // if there is no obj return the from object
+        var objKeys = _.keys(obj),
+            fromKeys = _.keys(from);
+
+        // return from because obj has not values
+        if(!objKeys.length)
+            return from;
+
+        // return obj because from has not values
+        if(!fromKeys.length)
+            return obj;
+
+        _.each(objKeys, function(key){
+
+            var value = from[key];
+
+            //
+            // set undefined default values
+            //
+            if( Type.of(value) == "undefined"){
+
+                switch(Type.of(obj[key])){
+
+                    case "boolean":
+                    case "number":
+                    case "function":
+                    case "string":
+                    case "array":
+                        // sets the default value to be the one from the left
+                        value = obj[key];
+                        break;
+
+                    default:
+                        value = null;
+                        break;
+                }
+            }
+
+            //
+            // rules for null values ( value default's )
+            // 
+            /* jshint -W041 */
+            if( value == null ){
+
+                switch(Type.of(obj[key])){
+
+                    case "string":
+                        value = "";
+                        break;
+
+                    case "array":
+                        value = [];
+                        break;
+
+                    case "object":
+                        value = {};
+                        break;
+
+                    
+                    // If the value is set to null what is suposed to be the 
+                    // default value?
+
+                    // I don't think that should be a default value when
+                    // the type should be a: number, boolean or function
+
+                }
+
+            }
+
+
+            //
+            // guards - make sure the value are from the same type
+            // if obj[key] is null ignore the testing
+            //
+            /* jshint -W041 */
+            if( obj[key] != null && Type.of(obj[key]) != Type.of(value) ){
+                throw new Error("[js-utils-lib/Arguments] " + key + ": expected type '" + Type.of(obj[key]) + "' but found '" + Type.of(from[key]) + "'");
+            }
+
+            //
+            // set values
+            //
+            /* jshint -W041 */
+            if(  obj[key] != null && Type.of(obj[key]) == "object"){
+                // if is object deep copy
+                var copy = extend(obj[key], from[key] || {});
+                obj[key] = copy;
+            }
+            else{
+                // set the real thing!
+                obj[key] = value;       
+            }
+
+        });
+
+
+        return obj;
+    };
+
+
 
     /*
      * take care of options operations on javascript
@@ -20,7 +141,7 @@ define(["lodash", "js-utils-lib/Type", "js-utils/DataStructures/Object", "js-uti
     var Arguments = {
 
         /*
-         * gets the options Object
+         * Get the argument of a function
          *
          * @param{options} The given options Hash
          * @param{defaultOptions} The defaults options Hash
@@ -35,19 +156,7 @@ define(["lodash", "js-utils-lib/Type", "js-utils/DataStructures/Object", "js-uti
             options = options || {};
 
             // safelly copy definitions
-            try{
-
-                options = Obj.fill(defaultOptions, options);    
-
-            }catch(e){
-
-                // always log the error thrown
-                log.e(e);
-
-                throw e;
-
-            }
-            
+            options = extend(defaultOptions, options);
 
             return options;
         }
--- src/js/ArgumentsSpec.js
+++ src/js/ArgumentsSpec.js
@@ -21,26 +21,185 @@ describe("ArgumentsSpec", function () {
     });
 
 
-/*
-    async.it(".get() should throw when expecting an array", function (done) {
+    async.it(".get() should not fill unwanted values", function (done) {
 
-        Injector.require(["js-utils-lib/Arguments.js"], function(Options){
+        Injector.require(["lodash", "js-utils-lib/Arguments"], function(_, Options){
+
+            var o = Options.get(
+                {
+                    "val": {
+                        "one": 1,
+                        "two": 2,
+                        "innerhash": { 1: 1, 2: 2 },
+                        "innerarray": [ 1, 2 ]
+                    },
+
+                    "val1": {
+                        "one": 1
+                    }
+
+                },
+                {
+                    "val": {
+                        "one": 1,
+                        "innerhash": {},
+                        "innerarray": []
+                    }   
+                }
+
+            );
+
+            expect(typeof o).toEqual("object");
+            
+            expect(o.val).not.toBe(null);
+            expect(o.val.one).not.toBe(null);
+
+            expect(_.keys(o.val.innerhash).length).toBe(2);
+            expect(_.keys(o.val.innerarray).length).toBe(2);
+            
+            expect(o.val1).toBe(undefined);
+            expect(o.val.two).toBe(undefined);
+
+            done();
+
+        });
+
+    });
+
+
+    async.it(".get() should ignore null values on source", function (done) {
+
+        Injector.require(["lodash", "js-utils-lib/Arguments"], function(_, Options){
+
+            var o = Options.get(
+                
+                null,
+
+                {
+                    "val": {
+                        "one": 1,
+                        "innerhash": {},
+                        "innerarray": []
+                    }   
+                }
+
+            );
+
+            expect(typeof o).toEqual("object");
+            
+            expect(o.val).not.toBe(null);
+            expect(o.val.one).toBe(1);
+
+            expect(_.keys(o.val.innerhash).length).toBe(0);
+            expect(_.keys(o.val.innerarray).length).toBe(0);
+            
+            done();
+
+        });
+
+    });
+
+
+
+    async.it(".get() should return source when obj is null", function (done) {
+
+        Injector.require(["lodash", "js-utils-lib/Arguments"], function(_, Options){
+
+            var o = Options.get(
+                
+                {
+                    "val": {
+                        "one": 1,
+                        "innerhash": {},
+                        "innerarray": []
+                    }   
+                },
+
+                null
+
+            );
+
+            expect(typeof o).toEqual("object");
+            
+            expect(o.val).not.toBe(null);
+            expect(o.val.one).toBe(1);
+
+            expect(_.keys(o.val.innerhash).length).toBe(0);
+            expect(_.keys(o.val.innerarray).length).toBe(0);
+            
+            done();
+
+        });
+
+    });
+
+
+    async.it(".get() should accept anything as null base values", function (done) {
+
+        Injector.require(["lodash", "js-utils-lib/Arguments"], function(_, Options){
+
+            var o = Options.get(
+                
+                {
+                    "val": {
+                        "one": 1,
+                        "two": [1, 2],
+                        "three": { 1: 1}
+                    }
+
+                },
+
+                {
+                    "val": {
+                        "one": null,
+                        "two": null,
+                        "three": null,
+                    }   
+                }
+
+            );
+
+            expect(typeof o).toEqual("object");
+            
+            expect(o.val).not.toBe(null);
+            
+            expect(o.val.one).toBe(1);
+            expect(_.keys(o.val.two).length).toBe(2);
+            expect(_.keys(o.val.three).length).toBe(1);
+            
+            done();
+
+        });
+
+    });
+
+
+
+    async.it(".get() should throw error when array expected", function (done) {
+
+        Injector.require(["js-utils-lib/Arguments"], function(Options){
 
             var run = function(){
+
                 var o = Options.get(
+                
+                    // default
                     {
                         "val": {
-                            "one": 1
+                            "one": {}
                         }
                     },
 
                     {
-                        "val": [] 
+                        "val": {
+                            "one": []
+                        }   
                     }
 
                 );
-            };
 
+            };
+            
             expect(run).toThrow();
             
             done();
@@ -48,7 +207,193 @@ describe("ArgumentsSpec", function () {
         });
 
     });
-*/
+
+
+    async.it(".get() should throw error when string expected", function (done) {
+
+        Injector.require(["js-utils-lib/Arguments"], function(Options){
+
+            var run = function(){
+
+                var o = Options.get(
+                
+                    // default
+                    {
+                        "val": {
+                            "one": {}
+                        }
+                    },
+
+                    {
+                        "val": {
+                            "one": ""
+                        }   
+                    }
+
+                );
+
+            };
+            
+            expect(run).toThrow();
+            
+            done();
+
+        });
+
+    });
+
+
+
+    async.it(".get() should throw error when number expected", function (done) {
+
+        Injector.require(["js-utils-lib/Arguments"], function(Options){
+
+            var run = function(){
+
+                var o = Options.get(
+                
+                    // default
+                    {
+                        "val": {
+                            "one": {}
+                        }
+                    },
+
+                    {
+                        "val": {
+                            "one": 1
+                        }   
+                    }
+
+                );
+
+            };
+            
+            expect(run).toThrow();
+            
+            done();
+
+        });
+
+    });
+
+
+
+    async.it(".get() should have the value from the default option when it's not specified", function (done) {
+
+        Injector.require(["js-utils-lib/Arguments"], function(Options){
+
+            var o = Options.get(
+            
+                {
+                    "val": {
+                        "one": 1
+                    }
+                },
+
+                {
+                    "val": {
+                        "one": 1,
+                        "two": 2
+                    }
+                }
+
+            );
+            
+            expect(o.val.two).toBe(2);
+            
+            done();
+
+        });
+
+    });
+
+
+    async.it(".get() should set the default value when a string field is null", function (done) {
+
+        Injector.require(["js-utils-lib/Arguments"], function(Options){
+
+            var o = Options.get(
+            
+                {
+                    "val": {
+                        "one": null
+                    }
+                },
+
+                {
+                    "val": {
+                        "one": "",
+                    }
+                }
+
+            );
+            
+            expect(o.val.one).toBe("");
+            
+            done();
+
+        });
+
+    });
+
+
+    async.it(".get() should set the default value when a object field is null", function (done) {
+
+        Injector.require(["js-utils-lib/Arguments"], function(Options){
+
+            var o = Options.get(
+            
+                {
+                    "val": {
+                        "one": null
+                    }
+                },
+
+                {
+                    "val": {
+                        "one": {},
+                    }
+                }
+
+            );
+            
+            expect(o.val.one).not.toBe(null);
+            
+            done();
+
+        });
+
+    });
+
+
+    async.it(".get() should set the default value when a array field is null", function (done) {
+
+        Injector.require(["js-utils-lib/Arguments"], function(Options){
+
+            var o = Options.get(
+            
+                {
+                    "val": {
+                        "one": null
+                    }
+                },
+
+                {
+                    "val": {
+                        "one": [],
+                    }
+                }
+
+            );
+            
+            expect(o.val.one).not.toBe(null);
+            
+            done();
+
+        });
+
+    });
 
 
 });
