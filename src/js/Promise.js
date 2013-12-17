8a87708d3c002dafc07bb5b8c9be099f487b6766
Oscar Brito <aetheon@gmail.com>
2013-12-17 16:10:16 +0000

    Added js/Promise

---
+++ src/js/Promise.js
@@ -0,0 +1,73 @@
+
+// share code between server / client
+if (typeof define !== 'function') { var define = require('amdefine')(module); }
+
+define([
+    "require", 
+
+    "lodash", 
+    "q",
+
+    "js-utils-lib/Type",
+    "js-utils-lib/Safe"
+
+    ], function(require){
+        "use strict";
+
+
+        /// using q as library
+        var Q = require("q"),
+            Type = require("js-utils-lib/Type"),
+            Safe = require("js-utils-lib/Safe");
+
+
+        var PromiseHelper = {
+
+
+            /**
+             * Runs a sequence of functions
+             * 
+             * @param  {*}      initialValue The initial argument for the function
+             * @param  {Array}  funcs        The array of functions
+             * @return {Promise}              
+             *
+             * @example
+             *
+             *  Promise.sequence(0, [fn,fn,fn]).then(function(total){});
+             *  Promise.sequence([fn,fn,fn]).then(function(total){});
+             *  
+             */
+            sequence: function(initialValue, funcs){
+
+                // order of the initialValue's
+                if(Type.isArray(initialValue)){
+                    funcs = initialValue;
+                    initialValue = null;
+                }
+
+                // make sure that funcs is an array
+                funcs = Safe.getArray(funcs);
+                
+                /* jshint -W064 */
+                var result = Q(initialValue);
+                _.each(
+                    funcs,
+                    function(f){
+                        // only execute is the result is a promise
+                        if(Q.isPromise(result)){
+                            result = result.then(f);
+                        }
+                    });
+
+                return result;
+
+            }
+
+
+        };
+
+
+        return PromiseHelper;
+
+
+    });
---
+++ src/js/PromiseSpec.js
@@ -0,0 +1,67 @@
+
+describe("SafeSpec", function () {
+
+    var Squire = null,
+        Injector = null,
+        async = new AsyncSpec(this);
+
+
+    async.beforeEach(function (done) {
+
+        require(["squire"], function(SquireLib){
+            
+            Squire = SquireLib;
+            Injector = new Squire();
+
+            done();
+
+        });
+
+    });
+
+
+    
+    async.it(".sequence(initialValue, [])", function (done) {
+
+        Injector.require(["q", "js-utils-lib/Promise"], function(Q, Promise){
+
+            var sum = function(val){ 
+                var dfd = Q.defer();
+                dfd.resolve(val + 1);
+                return dfd.promise;
+            };
+
+            Promise.sequence(0, [sum, sum, sum]).then(
+                function(total){
+                    expect(total).toBe(3);
+                    done();
+                });
+
+        });
+
+    });
+
+    async.it(".sequence([])", function (done) {
+
+        Injector.require(["q", "js-utils-lib/Promise"], function(Q, Promise){
+
+            var sum = function(val){ 
+                val = val || 0;
+                var dfd = Q.defer();
+                dfd.resolve(val + 1);
+                return dfd.promise;
+            };
+
+            Promise.sequence([sum, sum, sum]).then(
+                function(total){
+                    expect(total).toBe(3);
+                    done();
+                });
+
+        });
+
+    });
+
+
+
+});
