1f513f2877d5472879044ba3e642e57cddd44756
Oscar Brito <aetheon@gmail.com>
2014-01-23 09:48:26 +0000

    Added angular to libs

--- .require.js
+++ .require.js
@@ -21,6 +21,7 @@ requirejs.config({
 
         'bootstrap': 'lib/bootstrap/latest/js/bootstrap',
 
+        'angular': 'lib/angular/latest',
         'ember': 'lib/ember/ember-latest',
 
         'knockout': 'lib/knockout/knockout-latest',
